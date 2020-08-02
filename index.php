6eb42d15f6e61b2e33fbb4a058b777b9467a91f5
Aep <aepsaprul18@gmail.com>
2020-08-02 12:43:20 +0700

    instal bootstrap, font

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/custom.css">
+  <link href="https://fonts.googleapis.com/css2?family=Acme&family=Bebas+Neue&family=Play&display=swap" rel="stylesheet">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="col-md-12 warna-krim" style="">
+      <h1 class="text-center">gula gendis</h1>
+      <p class="text-center">Minumannya orang Indonesia</p>
+    </div>
+  </div>
+</body>
+</html>
